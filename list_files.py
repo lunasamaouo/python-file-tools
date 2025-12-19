9b4e5d5921de6e82cb07840045de3e3fc89b4bd8
lunasamaouo <133499472+lunasamaouo@users.noreply.github.com>
2025-12-19 17:00:51 +0800

    Add files via upload
    feat: add file extension detection功能：添加文件扩展名检测

--- list_files.py
+++ list_files.py
@@ -1,9 +1,16 @@
 import os
 
-folder_path = "text_folder"  # 替换为你的文件夹路径
+folder_path = "test_folder"
 
 files = os.listdir(folder_path)
 
 print("当前文件夹中的文件有：")
+
 for file in files:
-    print(file)
+    file_path = os.path.join(folder_path, file)
+
+    if os.path.isfile(file_path):
+        file_name, file_ext = os.path.splitext(file)
+        print(f"文件名：{file_name}，文件类型：{file_ext}")
+
+
