ede71ced04d9439a0c6600f68db1ef884fc005c3
小路 <xuan18998469721@outlook.com>
2025-12-20 22:58:44 +0800

    Add files via upload

--- list_files.py
+++ list_files.py
@@ -1,4 +1,16 @@
 import os
+import shutil
+
+source_folder = "test_folder"
+ 
+txt_folder = os.path.join(source_folder, "txt") # 创建txt文件夹路径
+jpg_folder = os.path.join(source_folder, "jpg") # 创建jpg文件夹路径
+other_folder = os.path.join(source_folder, "other") # 创建other文件夹路径
+
+os.makedirs(txt_folder, exist_ok=True)# 创建txt文件夹
+os.makedirs(jpg_folder, exist_ok=True)
+os.makedirs(other_folder, exist_ok=True)
+
 
 folder_path = "test_folder"
 
@@ -6,11 +18,25 @@ files = os.listdir(folder_path)
 
 print("当前文件夹中的文件有：")
 
-for file in files:
-    file_path = os.path.join(folder_path, file)
+for file in os.listdir(source_folder):
+    file_path = os.path.join(source_folder, file)
 
-    if os.path.isfile(file_path):
-        file_name, file_ext = os.path.splitext(file)
-        print(f"文件名：{file_name}，文件类型：{file_ext}")
+    if not os.path.isfile(file_path):
+        continue
+
+    file_name, file_ext = os.path.splitext(file)
+
+    if file_ext == ".txt":
+        target_folder = txt_folder
+    elif file_ext == ".jpg":
+        target_folder = jpg_folder
+    else:
+        target_folder = other_folder
+
+    target_path = os.path.join(target_folder, file)
+    shutil.move(file_path, target_path)
+
+    print(f"已移动文件：{file} → {target_folder}")
+        
 
 
